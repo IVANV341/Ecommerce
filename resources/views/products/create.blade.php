aa6ddde4083803c0364c74e90e58c28e3f79262b
CesarSolano <csolano@unab.edu.co>
2025-09-11 12:51:31 -0500

    Layouts basicos

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
+@extends('layouts.app')
+@section('content')
 <h1>Formulario para crear producto</h1>
 <form action="" method="post">
     <label for="name">Nombre:</label>
@@ -24,6 +18,4 @@
     <label for="brand">Brand:</label>
     <input type="text" name="brand">
 </form>
-
-</body>
-</html>
+@endsection
