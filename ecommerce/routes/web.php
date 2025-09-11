e1855030d9ab02146be3f1bb0d6d231388d2213f
ivanv <overa32@unab.edu.co>
2025-09-11 11:33:01 -0500

    controlador basico

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::profix('products')
+
+    Route::get('products', [ProductController::class, 'index']
+    );
+
+    Route::get('products/create',[ProductController::class, 'create']);
+
+    Route::get('products/{id}/{category?}',[ProductController::class, 'show']);
+        
+
